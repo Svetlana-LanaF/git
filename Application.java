fcc3e5f050a7e51e42774aa3e15d32ec99c0f3b3
svetlanafateeva <svetikf17@yandex.ru>
2018-09-27 10:05:29 +0400

    Added entry point

---
+++ Application.java
@@ -0,0 +1,7 @@
+package PO62.Fateeva.wdad;
+
+public class Application {
+    public static void main(String[] args) {
+        System.out.println("I'm Svetlana Fateeva, and I'm not a monkey");
+    }
+}
--- data/managers/firstclass.java
+++
@@ -1,7 +0,0 @@
-package PO62.Fateeva.wdad.data.managers;
-
-public class firstclass {
-    public static void main(String[] args) {
-
-    }
-}
